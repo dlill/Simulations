23fb4a28d041c9b3c45023ac320e0681f6d52d0f
dlill <daniel.lill@posteo.de>
2017-03-21 19:43:23 +0000

    Have got a stable version of Prabakaran.Rmd, which has a nice output. Also in Prabakaran/Mathematica, the adding states.nb file is nice, because it shows that the orthogonalization works for every Parameter in the erk module

---
+++ Models/Prabakaran/replaceregex.sh
@@ -0,0 +1,2 @@
+sed -r -i 's/m2COMPe1/Cm2e1/g' *.csv #-i = In file replacement, 's/regexp/sub/flag' = substitute regexp by sub, flag g = more than one per line, *.m = all files ending with .m
+sed -r -i 's/e2COMPp1/Ce2p1/g' *.csv 
---
+++ Models/replaceregex.sh
@@ -0,0 +1,2 @@
+sed -r -i 's/m2COMPe1/Cm2e1/g' *.csv #-i = In file replacement, 's/regexp/sub/flag' = substitute regexp by sub, flag g = more than one per line, *.m = all files ending with .m
+# sed -i 's/yCOMPz/y2COMPz1/g' *.csv 
