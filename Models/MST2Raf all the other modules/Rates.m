37840b97be1ef08bcaf2bbc32e6649481d47e33f
dlill <daniel.lill@posteo.de>
2017-04-05 14:55:23 +0100

    Changed Cascade.Rmd. Now, r_kept is again computed only with a single obs_fun which is supplied to it. Also, I tried some parameter values, but it doesn't work.

---
+++ Models/MST2Raf all the other modules/Rates.m
@@ -0,0 +1,25 @@
+(k1*RasGTP*pRaf1)/(Km1 + pRaf1)
+  (k2a*LATS1a*Raf1)/(Km2a + Raf1) +   (k2b*Kin*Raf1)/(Km2b + Raf1)
+ (V3*(1 + (Fa*ppERK)/Ka)*Raf1)/  ((1 + ppERK/Ka)*(Km3 + Raf1))
+ (V4*Raf1a)/(Km4 + Raf1a)
+  -(k5r*pMpR) + k5f*pMST2*pRaf1
+  -(k6r*MpR) + k6f*MST2*pRaf1
+ (AKTa*k7*(1 + Kact*RasGTP)*MST2)/  (Km7 + MST2)
+ (k8*PP2A*pMST2)/(Km8 + pMST2)
+ k9*MST2^2
+  (k10*PP2A*MST2a)/(Km10 + MST2a)
+ -(k11r*MaF1A) +   k11f*F1A*MST2a
+ -(k12r*MF1A) + k12f*F1A*MST2
+  (V13*MF1A)/(Km13 + MF1A)
+  (k14b*LATS1*MaF1A)/(Km14b + LATS1) + (k14a*LATS1*MST2a)/   (Km14a + LATS1)
+ (V15*LATS1a)/(Km15 + LATS1a)
+  k16af*MEK*Raf1a - k16ar*RaMk
+ k16b*RaMk
+  (V17*pMEK)/(Km17 + pMEK + (Km17*ppMEK)/Km19)
+  k18af*pMEK*Raf1a - k18ar*RapMk
+ k18b*RapMk
+  (V19*ppMEK)/(Km19 + (Km19*pMEK)/Km17 + ppMEK)
+  (k20*ERK*ppMEK)/(Km20 + ERK + (Km20*pERK)/Km22)
+  (V21*pERK)/(Km21 + (Km21*ERK)/Ki + pERK + (Km21*ppERK)/Km23)
+  (k22*pERK*ppMEK)/(Km22 + (Km22*ERK)/Km20 + pERK)
+  (V23*ppERK)/(Km23 + (Km23*ERK)/Ki + (Km23*pERK)/Km21 + ppERK)
---
+++ Models/MST2Raf all the other modules/xdot.m
@@ -0,0 +1,42 @@
+(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
+{k6r*MpR + k5r*pMpR - k6f*MST2*pRaf1 - k5f*pMST2*pRaf1 - 
+  (k1*RasGTP*pRaf1)/(Km1 + pRaf1) + (k2a*LATS1a*Raf1)/
+   (Km2a + Raf1) + (k2b*Kin*Raf1)/(Km2b + Raf1), 
+ (V3*(1 + (Fa*ppERK)/Ka)*Raf1)/((1 + ppERK/Ka)*(Km3 + Raf1)) - 
+  k16af*MEK*Raf1a - k18af*pMEK*Raf1a - 
+  (V4*Raf1a)/(Km4 + Raf1a) + k16ar*RaMk + k16b*RaMk + 
+  k18ar*RapMk + k18b*RapMk, 
+ (AKTa*k7*(1 + Kact*RasGTP)*MST2)/(Km7 + MST2) + k5r*pMpR - 
+  (k8*PP2A*pMST2)/(Km8 + pMST2) - k5f*pMST2*pRaf1, 
+ k12r*MF1A + k6r*MpR - k12f*F1A*MST2 - 2*k9*MST2^2 - 
+  (AKTa*k7*(1 + Kact*RasGTP)*MST2)/(Km7 + MST2) + 
+  (k10*PP2A*MST2a)/(Km10 + MST2a) + (k8*PP2A*pMST2)/
+   (Km8 + pMST2) - k6f*MST2*pRaf1, k11r*MaF1A + 2*k9*MST2^2 - 
+  k11f*F1A*MST2a - (k10*PP2A*MST2a)/(Km10 + MST2a), 
+ -(k11r*MaF1A) + (V13*MF1A)/(Km13 + MF1A) + k11f*F1A*MST2a, 
+ -(k12r*MF1A) - (V13*MF1A)/(Km13 + MF1A) + k12f*F1A*MST2, 
+ k11r*MaF1A + k12r*MF1A - k12f*F1A*MST2 - k11f*F1A*MST2a, 
+ (V15*LATS1a)/(Km15 + LATS1a) - (k14b*LATS1*MaF1A)/
+   (Km14b + LATS1) - (k14a*LATS1*MST2a)/(Km14a + LATS1), 
+ -((V15*LATS1a)/(Km15 + LATS1a)) + (k14b*LATS1*MaF1A)/
+   (Km14b + LATS1) + (k14a*LATS1*MST2a)/(Km14a + LATS1), 
+ (V17*pMEK)/(Km17 + pMEK + (Km17*ppMEK)/Km19) - 
+  k16af*MEK*Raf1a + k16ar*RaMk, 
+ (V19*ppMEK)/(Km19 + (Km19*pMEK)/Km17 + ppMEK) - 
+  (V17*pMEK)/(Km17 + pMEK + (Km17*ppMEK)/Km19) - 
+  k18af*pMEK*Raf1a + k16b*RaMk + k18ar*RapMk, 
+ (V21*pERK)/(Km21 + (Km21*ERK)/Ki + pERK + (Km21*ppERK)/Km23) - 
+  (k20*ERK*ppMEK)/(Km20 + ERK + (Km20*pERK)/Km22), 
+ (V23*ppERK)/(Km23 + (Km23*ERK)/Ki + (Km23*pERK)/Km21 + ppERK) - 
+  (V21*pERK)/(Km21 + (Km21*ERK)/Ki + pERK + (Km21*ppERK)/Km23) - 
+  (k22*pERK*ppMEK)/(Km22 + (Km22*ERK)/Km20 + pERK) + 
+  (k20*ERK*ppMEK)/(Km20 + ERK + (Km20*pERK)/Km22), 
+ -((V23*ppERK)/(Km23 + (Km23*ERK)/Ki + (Km23*pERK)/Km21 + 
+     ppERK)) + (k22*pERK*ppMEK)/(Km22 + (Km22*ERK)/Km20 + 
+    pERK), -(k5r*pMpR) + k5f*pMST2*pRaf1, 
+ -(k6r*MpR) + k6f*MST2*pRaf1, 
+ (k1*RasGTP*pRaf1)/(Km1 + pRaf1) - (k2a*LATS1a*Raf1)/
+   (Km2a + Raf1) - (k2b*Kin*Raf1)/(Km2b + Raf1) - 
+  (V3*(1 + (Fa*ppERK)/Ka)*Raf1)/((1 + ppERK/Ka)*(Km3 + Raf1)) + 
+  (V4*Raf1a)/(Km4 + Raf1a), k16af*MEK*Raf1a - k16ar*RaMk - 
+  k16b*RaMk, k18af*pMEK*Raf1a - k18ar*RapMk - k18b*RapMk}
