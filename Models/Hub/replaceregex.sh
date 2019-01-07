ad586217f3455bd5c28e1ac0336643c11b164a6f
Daniel Lill <daniel.lill@posteo.de>
2019-01-07 14:40:23 +0100

    Cleanup

--- Models/Hub/replaceregex.sh
+++
@@ -1,2 +0,0 @@
-sed -r -i 's/x2COMPy1/Cx2y1/g' *.csv #-i = In file replacement, 's/regexp/sub/flag' = substitute regexp by sub, flag g = more than one per line, *.m = all files ending with .m
-sed -r -i 's/y2COMPz1/Cy2z1/g' *.csv 
