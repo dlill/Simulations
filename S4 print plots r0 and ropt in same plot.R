9405cbec894868510be8266be869b8c8b008eee5
Daniel Lill <daniel.lill@posteo.de>
2019-01-10 17:27:28 +0100

    Noise script ready

--- S4 print plots r0 and ropt in same plot.R
+++ S4 print plots r0 and ropt in same plot.R
@@ -18,11 +18,9 @@ p1 <-
     theme(plot.title = element_text(hjust = 0.5)) +
     geom_blank()
   
-  # ggsave(paste0("boxcombined_srel-", s, "_deltap-", round(exp(perturbation_strength),2), ".png"), p1)
-  # ggsave(paste0("boxcombined_srel-", s, "_deltap-", round(exp(perturbation_strength),2), "zoom.png"), p1 + 
-  #          coord_cartesian(ylim = c(-2,2)))
-out <- tibble(s = s, p = perturbation_strength, plot = list(p1))
-return(out)
+  ggsave(paste0("boxcombined_srel-", s, "_deltap-", round(exp(perturbation_strength),2), ".png"), p1)
+  ggsave(paste0("boxcombined_srel-", s, "_deltap-", round(exp(perturbation_strength),2), "zoom.png"), p1 +
+           coord_cartesian(ylim = c(-2,2)))
 }
 
 asp <- expand.grid(a = 0, s = map_dbl(error_pars, 2), perturbation_strength = map_dbl(perturbations,1))
@@ -30,23 +28,11 @@ asp <- expand.grid(a = 0, s = map_dbl(error_pars, 2), perturbation_strength = ma
 # dir.create("~/Promotion/Projects/MRA/Simulations/Models/Cascade/Noise/plots")
 setwd("~/Promotion/Projects/MRA/Simulations/Models/Cascade/Noise/plots")
 
-# walk(seq_len(nrow(asp)), function(.x) {
-#   # pdf(paste0("boxplots_srel_",asp[.x,]$s, "delta_p_", round(exp(asp[.x,]$perturbation_strength),2), "_alpha_", asp[.x,]$a, ".pdf"))
-#   with(asp[.x,], print_distribution_plots2(results_unnested_matrices, s, perturbation_strength))
-#   # dev.off()
-# })
-
-
-
-plotframe <- map(seq_len(nrow(asp)), function(.x) {
-  # pdf(paste0("boxplots_srel_",asp[.x,]$s, "delta_p_", round(exp(asp[.x,]$perturbation_strength),2), "_alpha_", asp[.x,]$a, ".pdf"))
+walk(seq_len(nrow(asp)), function(.x) {
   with(asp[.x,], print_distribution_plots2(results_unnested_matrices, s, perturbation_strength))
-  # dev.off()
-}) %>% bind_rows()
+})
 
-dev.new()
-par(mfrow = c(2,2))
-wup <- plotframe %>% filter(near(p, log(0.5))) %>% 
-  .$plot %>% .[[1]] %>% 
-  map(print)
-dev.off()
+
+
+
+
